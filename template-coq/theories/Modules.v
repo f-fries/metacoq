424ae40f18f068c44f2c6f744c128f69942c7550
Fabian Fries <s8fafrie@stud.uni-saarland.de>
2020-12-09 16:18:20 +0100

    fixed Constants + added <Modules> for covinience

---
+++ template-coq/theories/Modules.v
@@ -0,0 +1,78 @@
+(* This file provides the previous interface to the plugin
+*
+* Previously:
+*   From MetaCoq.Template Require Import A B C.
+*
+* Now:
+*   From MetaCoq.Template Require Modules.
+*   Import A B C.
+*)
+
+From MetaCoq.Template Require Import
+    Ident
+    BasicAst
+    Universes
+    uGraph
+    Environment
+    EnvironmentTyping
+    Ast
+    AstUtils
+    Pretty
+    utils
+    monad_utils
+    Pretty
+    TemplateMonad
+    LiftSubst
+    UnivSubst
+    TemplateMonad.
+
+
+(* For the stuff that's shared with PCUIC, export the versions that are used by the TemplateCoq *)
+Module BasicAst := BasicAst.Native.
+Module Universes := Universes.Native.
+Module uGraph := uGraph.Native.
+Module Environment := Environment.Native.
+Module EnvironmentTyping := EnvironmentTyping.Native.
+
+(* Provide the other Module for convinience *)
+Module Ast := Ast.
+Module Astutils := AstUtils.
+Module utils := utils.
+Module monad_utils := monad_utils.
+Module Pretty := Pretty.
+Module TemplateMonad := TemplateMonad.
+Module LiftSubst := LiftSubst.
+Module UnivSubst := UnivSubst.
+Module TemplateMonad := TemplateMonad.
+
+Module All.
+Export
+    Ident
+    BasicAst
+    Universes
+    uGraph
+    Environment
+    EnvironmentTyping
+    Ast
+    AstUtils
+    Pretty
+    utils
+    monad_utils
+    Pretty
+    TemplateMonad
+    LiftSubst
+    UnivSubst.
+    TemplateMonad
+End All.
+
+
+    
+
+
+    
+
+
+
+    
+
+    
